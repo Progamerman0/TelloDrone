a8aceb030b2a86ee5eca306ecc927744dceb378d
Spencer <spencerkohler52507@gmail.com>
2021-11-18 09:53:49 -0800

    Square function stable

--- TelloTemplate (test fights).py
+++ TelloTemplate (test fights).py
@@ -43,10 +43,16 @@ recvThread.start()
 
 # CREATE FUNCTIONS HERE....
 
+#Square
+def square():
+    sendmsg("up 75")
+    for i in range(4):
+        sendmsg("forward 100")
+        sendmsg("cw 90")
 
-print("\nFirst & Last Names")
-print("Program Name: ")
-print("Date: ")
+print("\nSpencer Kohler")
+print("Program Name: Test Flight")
+print("Date: 11-17-2021")
 print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
 print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
 ready = input('\nAre you ready to take flight: ')
@@ -59,8 +65,7 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Review the (SDK) Software Development Kit resource for Drone Commands
-        # Delete these comments before writing your program
+        square()
 
         sendmsg('land')
 
